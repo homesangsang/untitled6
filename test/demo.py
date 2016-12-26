4bb9c0b49611aa0078a3313da60726e29aa8771f
homesangsang <homesangsang@qq.com>
2016-12-26 22:43:15 +0800

    添加了检查输入信息的功能

--- test/demo.py
+++ test/demo.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from Tkinter import *
+from tkMessageBox import *
 import ttk
 import sqlite
 import sys
@@ -7,40 +8,51 @@ import sys
 # 避免数据库中文乱码
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
 conn = sqlite.connect("studentdbadmin.db")
+
 cursor = conn.cursor()
 
 
+def check(character):
+    for char in character:
+        if not(char>= '0' and char<= '9'):
+            showerror(title='错误提示', message='输入信息不合法')
+            return 0
+    return 1
+
 def query(username):  # 查询学生信息
-    sql = "select * from stu_message where name= '" + username + "'"
-    cursor.execute(sql)
-    rs = cursor.fetchone()
-    if rs:
-        print rs[0]
-        bishi = float(rs[1])
-        shangji = float(rs[2])
-        score = bishi + shangji
-        if score < 120 :
-            print "不及格"
-            clearmessage()
-            exam_res.insert(0, '不及格')
-        elif shangji < 60 :
-            print "全部重考"
-            clearmessage()
-            exam_res.insert(0, '全部重考')
-        elif bishi < 60 :
-            print "笔试重考"
-            clearmessage()
-            exam_res.insert(0, '笔试重考')
+    if check(username) == 1:
+        sql = "select * from stu_message where name= '" + username + "'"
+        cursor.execute(sql)
+        rs = cursor.fetchone()
+        if rs:
+            print rs[0]
+            bishi = float(rs[1])
+            shangji = float(rs[2])
+            score = bishi + shangji
+            if score < 120 :
+                print "不及格"
+                clearmessage()
+                exam_res.insert(0, '不及格')
+            elif shangji < 60 :
+                print "全部重考"
+                clearmessage()
+                exam_res.insert(0, '全部重考')
+            elif bishi < 60 :
+                print "笔试重考"
+                clearmessage()
+                exam_res.insert(0, '笔试重考')
+            else:
+                print "考试通过!!!"
+                clearmessage()
+                exam_res.insert(0, '考试通过!!!')
+            pass
         else:
-            print "考试通过!!!"
             clearmessage()
-            exam_res.insert(0, '考试通过!!!')
-        pass
-    else:
-        clearmessage()
-        exam_res.insert(0, '数据库没有该考生的信息')
+            exam_res.insert(0, '数据库没有该考生的信息')
+
+
+
 
 def insert(name, bishi, shangji):     # 插入一条记录
     sql = "insert into stu_message VALUES ('"+name+"',"+bishi+","+shangji+")"
@@ -49,9 +61,11 @@ def insert(name, bishi, shangji):     # 插入一条记录
         cursor.execute(sql)
         # 提交到数据库执行
         conn.commit()
+        showinfo("successful", "成功插入一条消息")
     except:
         # 如果执行不成功则回滚
         conn.rollback()
+
 
 def clearmessage():      # 清楚表格中的信息
     uname_entry.delete(0, END)
@@ -60,6 +74,7 @@ def clearmessage():      # 清楚表格中的信息
     shangji_input.delete(0, END)
     bishi_input.delete(0, END)
     pass
+
 
 def calculate(*args):
     try:
@@ -74,11 +89,13 @@ def hellobuttontest():  # 获取文本框(Entry)的值,获取学号和姓名
     print uname_entry_value
     query(uname_entry_value)
 
+
 def insertrecord():
     uname = uname_input.get()
     shangji = shangji_input.get()
     bishi = bishi_input.get()
-    insert(uname, bishi, shangji)
+    if check(shangji) ==1 and check(bishi)==1:
+        insert(uname, bishi, shangji)
 
 
 root = Tk()
@@ -104,17 +121,17 @@ ttk.Button(mainframe, text="查询", command=hellobuttontest).grid(column=2, row
 ttk.Button(mainframe, text="重置", command=clearmessage).grid(column=3, row=3, sticky=W)
 
 # 用户信息录入模块
-ttk.Label(mainframe, text="用户信息录入").grid(column=1, row=4, columnspan=3, sticky=(W,E))
+ttk.Label(mainframe, text="用户信息录入").grid(column=1, row=4, columnspan=3, sticky=(W, E))
 
-ttk.Label(mainframe, text="用户名").grid(column=1, row=5, sticky=(W,E))
+ttk.Label(mainframe, text="用户名").grid(column=1, row=5, sticky=(W, E))
 uname_input = ttk.Entry(mainframe, width=10)
-uname_input.grid(column=2, row=5, columnspan=2, sticky=(W,E))
-ttk.Label(mainframe, text="笔试成绩").grid(column=1, row=6, sticky=(W,E))
+uname_input.grid(column=2, row=5, columnspan=2, sticky=(W, E))
+ttk.Label(mainframe, text="笔试成绩").grid(column=1, row=6, sticky=(W, E))
 bishi_input = ttk.Entry(mainframe, width=10)
-bishi_input.grid(column=2, row=6, columnspan=2,sticky=(W,E))
-ttk.Label(mainframe, text="上机成绩").grid(column=1, row=7, sticky=(W,E))
+bishi_input.grid(column=2, row=6, columnspan=2, sticky=(W, E))
+ttk.Label(mainframe, text="上机成绩").grid(column=1, row=7, sticky=(W, E))
 shangji_input = ttk.Entry(mainframe, width=10)
-shangji_input.grid(column=2, row=7, columnspan=2, sticky=(W,E))
+shangji_input.grid(column=2, row=7, columnspan=2, sticky=(W, E))
 ttk.Button(mainframe, text="录入", command=insertrecord).grid(column=2, row=8, sticky=W)
 ttk.Button(mainframe, text="重置", command=clearmessage).grid(column=3, row=8, sticky=W)
 
